005d308252e54f46dea6ae47a4bcb1f87f2ad028
Hannah <dh207443@dunnhumby.com>
2020-12-22 10:43:52 +0000

    Initialised homepage

---
+++ src/Homepage/Homepage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const HomePage = () => {
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content"> 
+                    <h1> MENS </h1>
+                    <span>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content"> 
+                    <h1> WOMENS </h1>
+                    <span>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+}
